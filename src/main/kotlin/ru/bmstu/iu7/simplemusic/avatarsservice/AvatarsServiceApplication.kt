7cb24be019d8981f524e41ace48a293f67742329
denis96z <zinoviev.denis@yandex.ru>
2019-01-15 19:32:11 +0300

    initial commit

---
+++ src/main/kotlin/ru/bmstu/iu7/simplemusic/avatarsservice/AvatarsServiceApplication.kt
@@ -0,0 +1,11 @@
+package ru.bmstu.iu7.simplemusic.avatarsservice
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AvatarsServiceApplication
+
+fun main(args: Array<String>) {
+    runApplication<AvatarsServiceApplication>(*args)
+}
