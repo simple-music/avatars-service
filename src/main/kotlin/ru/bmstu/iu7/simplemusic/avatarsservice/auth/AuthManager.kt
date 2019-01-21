72e40c8d9256b7c33b3909568e5c0dcfdf20fb5e
denis96z <zinoviev.denis@yandex.ru>
2019-01-21 05:10:44 +0300

    gateway auth added

---
+++ src/main/kotlin/ru/bmstu/iu7/simplemusic/avatarsservice/auth/AuthManager.kt
@@ -0,0 +1,66 @@
+package ru.bmstu.iu7.simplemusic.avatarsservice.auth
+
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.stereotype.Component
+import java.util.*
+import kotlin.random.Random
+
+
+interface AuthManager {
+    fun checkToken(token: String): Boolean
+    fun getToken(id: String, password: String): String?
+}
+
+@Component
+class AuthManagerImpl(
+        @Value("\${security.service-id}")
+        private val serviceId:  String,
+
+        @Value("\${security.service-password}")
+        private val servicePassword:  String,
+
+        @Value("\${security.token-lifetime}")
+        private val tokenLifetime:  Int) : AuthManager {
+    private var token = this.generateToken()
+    private var tokenExpTime = this.countTokenExpTime()
+
+    override fun checkToken(token: String): Boolean {
+        synchronized(lock = this) {
+            return token == this.token
+        }
+    }
+
+    override fun getToken(id: String, password: String): String? {
+        synchronized(lock = this) {
+            if (id != this.serviceId || password != this.servicePassword) {
+                return null
+            }
+            if (Date() > this.tokenExpTime) {
+                this.token = this.generateToken()
+                this.tokenExpTime = this.countTokenExpTime()
+            }
+            return this.token
+        }
+    }
+
+    private fun generateToken(): String {
+        return (1..TOKEN_LENGTH)
+                .map {
+                    Random.nextInt(from = 0, until = charPool.size)
+                }
+                .map(charPool::get)
+                .joinToString(separator = "")
+    }
+
+    private fun countTokenExpTime(): Date {
+        val date = Calendar.getInstance()
+        return Date(date.timeInMillis + this.tokenLifetime * TIME_MINUTE)
+    }
+
+    private companion object {
+        private const val TOKEN_LENGTH = 100
+        private const val TIME_MINUTE = 60000
+        private val charPool : List<Char> =
+                ('a'..'z') + ('A'..'Z') + ('0'..'9')
+    }
+}
